0fa6bba26f33ade3e1c610bebd80564595dfebab
M4YERLY <166958760+M4YERLY@users.noreply.github.com>
2024-11-12 13:27:39 -0400

    Delete vista directory

--- vista/clientes/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_unset(); // Elimina todas las variables de sesión
-session_destroy(); // Destruye la sesión
-header("Location: ../usuarios/login.php"); // Redirige al login
-exit();
